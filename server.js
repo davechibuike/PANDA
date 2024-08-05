e132cd17c54a9ba6195e3e92d381d70397dcc77a
Dave Chibuike <chibike.dave@gmail.com>
2024-08-05 08:43:35 +0100

    added comment to connect to DB

--- server.js
+++ server.js
@@ -25,3 +25,6 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
+
+//Connect To DB :-
+// mongodb+srv://chibuikedaveuk:4tH4WZ4Z0y5mQz9F@natours.meodjwt.mongodb.net/
